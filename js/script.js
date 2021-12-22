ce804dccca936932616728bdfcc460e59e7a0eda
Conor Clarke <31772275+conorc470@users.noreply.github.com>
2021-12-22 09:47:25 +0000

    final commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const form = document.getElementById('form');
-const username = document.getElementById('username');
+const parentName = document.getElementById('parentName');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const phoneNumber = document.getElementById('phoneNumber');
+const childName = document.getElementById('childName');
 
 // Show input error message
 function showError(input, message) {
@@ -39,30 +40,6 @@ function checkRequired(inputArr) {
   });
 }
 
-// Check input length
-function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters`
-    );
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be less than ${max} characters`
-    );
-  } else {
-    showSuccess(input);
-  }
-}
-
-// Check passwords match
-function checkPasswordsMatch(input1, input2) {
-  if (input1.value !== input2.value) {
-    showError(input2, 'Passwords do not match');
-  }
-}
-
 // Get fieldname
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -72,9 +49,6 @@ function getFieldName(input) {
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  checkRequired([username, email, password, password2]);
-  checkLength(username, 3, 15);
-  checkLength(password, 6, 25);
+  checkRequired([parentName, email, password, phoneNumber, childName]);
   checkEmail(email);
-  checkPasswordsMatch(password, password2);
 });
